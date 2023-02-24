895c7a11024a0ecb097579fa918b4216b239f63b
Luma Development <12071794+LumaDevelopment@users.noreply.github.com>
2023-02-24 00:47:21 -0500

    Clean up unused usings and reference.

--- Course.cs
+++ Course.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Configuration;
-using System.Json;
 
 namespace CourseDistributor
 {
--- Program.cs
+++ Program.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Json;
-using System.Text.Json;
 
 namespace CourseDistributor
 {
